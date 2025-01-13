bf1804ceffe295d1271aa777401c76806c3542c3
Ben Holleran <ben.holleran@tenasol.com>
2025-01-13 12:38:01 -0500

    Adding new icon, and react-boostrap

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import logo from './Logo/Price Calculator Logo.png';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <img src={logo} className="App-logo" alt="Price Calculator Logo"/>
+    return (<div className="App">
+        <div className="App-header">
+                <span>
+                    <img src={logo} className="App-logo" alt="Price Calculator Logo" width="40px"/>
                     <h1>
                         Price Calculator
                     </h1>
-                </div>
-            </header>
+                </span>
         </div>
-    );
+        <div>
+            asdf
+        </div>
+    </div>);
 }
 
 export default App;
