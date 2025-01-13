e5d45518258126e5a018b69f6f7bf49f2c19d5b1
Ben Holleran <ben.holleran@tenasol.com>
2025-01-13 14:23:26 -0500

    Data loaded

--- src/data.js
+++ src/data.js
@@ -1,22 +1,12 @@
-
 export function getVmPriceData(setVmPriceData) {
-    fetch("./data/machine-types-regions.csv").then(
-        response => response.text()
-    ).then(
-        text => {
-            const lines = text.split("\n");
-            const data = [];
-            const headers = lines[0].split(",");
-            for (let i = 1; i < lines.length; i++) {
-                const values = lines[i].split(",");
-                const obj = {};
-                for (let j = 0; j < values.length; j++) {
-                    obj[headers[j]] = values[j];
-                }
-                data.push(obj);
-            }
-            console.log("loaded data", data);
+    fetch("./data/price_data.json").then(response => response.json()).then(
+        data => {
             setVmPriceData(data);
+        }).then(() => {
+            console.log("Data loaded");
         }
-    )
+    ).catch(error => {
+        console.error("Error loading data", error);
+    });
+
 }
